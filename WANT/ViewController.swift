6d33b0fa4830ee12702fe13aa7445e94f0593fcd
Chigusa Izumi <chigusa.f124@gmail.com>
2020-05-23 08:49:42 +0900

    Initial Commit

---
+++ WANT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WANT
+//
+//  Created by 泉 千種 on 2020/05/23.
+//  Copyright © 2020 Izumi zu-mi-. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
